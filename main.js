0f1ab1116a4c9e04bedb7fd9926c7f6951df2af0
De-Awan <dewagede0605@gmail.com>
2024-12-05 12:23:16 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $(".testimonials-container").owlCarousel({
+   
+      margin: 10,
+      responsive: {
+        0: {
+          items: 1,
+       
+        },
+        764: {
+          items: 2
+        },
+        1024: {
+          items: 3
+        },
+      },
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".testimonial-card");
+  
+    cards.forEach((card) => {
+      card.addEventListener("click", () => {
+        cards.forEach((c) => {
+          c.classList.remove("active");
+          c.querySelector(".quote img").src = "Quote copy.svg"; // Ubah ke quote copy ketika tidak aktif
+        });
+        card.classList.add("active");
+        card.querySelector(".quote img").src = "Quote.svg"; // Ubah ke quote ketika aktif
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            cards.forEach(c => c.classList.remove('active'));
+            card.classList.add('active');
+        });
+    });
+});
